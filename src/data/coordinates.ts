592c0707cab4c9ed696bfa6e22d2fc95d3054c5a
Eduardo Yubero Lavandeira <33625448+EduYube@users.noreply.github.com>
2026-08-04 15:01:29 +0200

    MAP-005 format campaign data files

--- src/data/coordinates.ts
+++ src/data/coordinates.ts
@@ -2,9 +2,8 @@ import type { CampaignCoordinate } from './model';
 
 export type LeafletSimpleCoordinate = readonly [number, number];
 
-export function toLeafletSimpleCoordinate({
-  x,
-  y,
-}: CampaignCoordinate): LeafletSimpleCoordinate {
+export function toLeafletSimpleCoordinate(
+  { x, y }: CampaignCoordinate,
+): LeafletSimpleCoordinate {
   return [y, x];
 }
