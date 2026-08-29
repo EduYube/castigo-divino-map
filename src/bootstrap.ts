f9e4f439c61411f4ffae367f135f627bdc4d2909
Eduardo Yubero Lavandeira <33625448+EduYube@users.noreply.github.com>
2026-08-29 16:00:53 +0200

    MAP-058 remove obsolete association sidecar import

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,4 @@
 import './app/adminCampaignScopeInterceptor';
 import './app/adminAuthEmailCallbackEntry';
 import './app/pinPlayerAssociationVisuals';
-import './app/adminEntityPlayerAssociations';
 import './main';
